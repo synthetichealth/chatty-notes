a29118b5aa43b03f4b86c8d5405775b0a0e07034
Andy Gregorowicz <andrewg@mitre.org>
2023-05-26 15:57:09 -0400

    Adding death cert generation and output to file

--- chatty.py
+++ chatty.py
@@ -2,7 +2,9 @@ import os
 import openai
 import argparse
 import json
-from base64 import b64decode
+from copy import deepcopy
+from time import sleep
+from base64 import b64encode
 from pathlib import Path
 from datetime import date
 from jinja2 import Environment, PackageLoader, select_autoescape
@@ -43,6 +45,10 @@ def extract_patient(bundle):
     patient_entry = next(filter(lambda be: be['resource']['resourceType'] == 'Patient', bundle['entry']))
     return patient_entry['resource']
 
+def extract_document_reference(bundle, reference_id):
+    dr_entry = next(filter(lambda be: be['resource']['id'] == reference_id, bundle['entry']))
+    return dr_entry['resource']
+
 def find_encounter(bundle, document_reference):
     encounter_id = document_reference['context']['encounter'][0]['reference']
     encounter_entry = next(filter(lambda be: be['fullUrl'] == encounter_id, bundle['entry']))
@@ -66,7 +72,6 @@ def procedure_display(procedure_list):
         raw_name = proc['code']['coding'][0]['display']
         procedures.append(raw_name.removesuffix('(procedure)').lower().strip())
     return procedures
-
 
 def extract_race(patient):
     race_extension = next(filter(lambda pe: pe['url'] == US_CORE_RACE, patient['extension']))
@@ -116,31 +121,68 @@ def create_template_environment():
     return Environment(loader=PackageLoader("chatty"),
                        autoescape=select_autoescape(), trim_blocks=True)
 
+def generate_note(prompt, role):
+    # Try to call ChatGPT 4 times for this note
+    for attempt in range(1, 5):
+        try:
+            response = openai.ChatCompletion.create(
+                model = "gpt-3.5-turbo",
+                messages = [
+                    {"role": "system", "content": role},
+                    {"role": "user", "content": prompt}
+                ]
+            )
+            ai_generated_note = response['choices'][0]['message']['content']
+            return ai_generated_note
+        except openai.error.RateLimitError:
+            # Sleep longer with each unsuccessful attempt to call the API
+            sleep(5 * attempt)
+    raise RuntimeError('Unable to generate note after 4 tries.')
+
+def write_output(input_file_name, output_bundle):
+    if not os.path.isdir('output'):
+        os.mkdir('output')
+    basename = os.path.basename(input_file_name)
+    output_path = Path('output') / basename
+    with open(output_path, "w") as outfile:
+        json.dump(output_bundle, outfile)
+
 def main():
     args = parse_arguments()
     bundle = parse_bundle_json(args.bundle)
+    # Create a copy of the bundle to write to file so we don't have to mutate
+    # the original.
+    output_bundle = deepcopy(bundle)
     patient = extract_patient(bundle)
     template_env = create_template_environment()
     openai.api_key = os.getenv("OPENAI_API_KEY")
     encounter_for_problem_template = template_env.get_template('encounter_for_problem.txt.jinja')
     er_template = template_env.get_template('emergency_room.txt.jinja')
+    death_cert_template = template_env.get_template('death_certification.txt.jinja')
     for dr in extract_resources_by_type(bundle, 'DocumentReference'):
         encounter = find_encounter(bundle, dr)
         context = build_template_context(patient, encounter, bundle)
         prompt = None
+        system_role = "You are a medical scribe."
         if encounter['type'][0]['coding'][0]['code'] == '50849002':
           prompt = er_template.render(context)
+        elif encounter['type'][0]['coding'][0]['code'] == '308646001':
+          prompt = death_cert_template.render(context)
+          system_role = "You are a medical examiner."
         elif 'reasonCode' in encounter:
           prompt = encounter_for_problem_template.render(context)
+
         if prompt is not None:
-          response = openai.ChatCompletion.create(
-              model="gpt-3.5-turbo",
-              messages=[
-                  {"role": "system", "content": "You are a medical scribe."},
-                  {"role": "user", "content": prompt}
-              ]
-          )
-          print(response['choices'][0]['message'])
+          ai_generated_note = generate_note(prompt, system_role)
+          # Calling decode at the end here may seem odd, but what it does is
+          # transform a python byte array into a string. The end result is
+          # the ChatGPT output as a Base64 encoded string.
+          encoded_note = b64encode(ai_generated_note.encode('utf-8')).decode()
+          reference_id = dr['id']
+          output_dr = extract_document_reference(output_bundle, reference_id)
+          output_dr['content'][0]['attachment']['data'] = encoded_note
+          sleep(15)
+    write_output(args.bundle, output_bundle)
 
 if __name__ == "__main__":
     main()
